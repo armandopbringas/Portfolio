9fd8dc65a6496642629ac3b0a419d17fd5d59a38
armanopbringas <bringas.armandop@gmail.com>
2021-03-16 00:10:27 -0600

    update portfolio

--- src/Data/dataContent.js
+++ src/Data/dataContent.js
@@ -2,41 +2,33 @@ module.exports = {
   aboutDescription: `My name is Armando, i´m a JavaScript developer in training. 
   I achived get my first job learning to code of self-thaught way but right there i 
   realized that i had too much for learn yet. I was a trainee frontend developer 
-  at Iceberg9 from jun 2020 to november 2020 collaborating as a remote way. Currently 
+  at Iceberg9 from august 2020 to december 2020 collaborating as a remote way. Currently 
   i find me in training in a code academy called Bedu, with the objetive of becomingme 
   in a better developer.`,
   skills: [
     {
       name: 'HTML 5',
-      level: 30
+      level: 40
     },
     {
       name: 'CSS 3',
+      level: 40
+    },
+    {
+      name: 'JavaScript',
+      level: 35
+    },
+    {
+      name: 'ReactJS',
       level: 30
     },
     {
-      name: 'Bootstrap 4',
-      level: 20
-    },
-    {
-      name: 'Sass',
-      level: 20
-    },
-    {
-      name: 'JavaScript - ES6',
+      name: 'NodeJS',
       level: 25
-    },
-    {
-      name: 'Webpack',
-      level: 20
-    },
-    {
-      name: 'React JS',
-      level: 20
     },
     {
       name: 'Git',
-      level: 25
+      level: 35
     }
   ]
 }
